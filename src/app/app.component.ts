5ab68ec12eacdd9a46981bcde8f06e781c31c5d0
Whalter Duarte <whalterdev@gmail.com>
2024-06-05 22:25:54 -0300

    home finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { ContainerComponent } from './container/container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, ContactComponent],
+  imports: [RouterOutlet, NavbarComponent, ContactComponent , ContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
